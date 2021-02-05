fde9dc9fb65fef72cf595eb342cab84c2433a1e9
sreelakshmi665 <16691a0589@mits.ac.in>
2021-02-05 10:07:24 +0530

    commited again!

--- gitChallenge4/src/gitChallenge4/Challenge4.java
+++ gitChallenge4/src/gitChallenge4/Challenge4.java
@@ -2,8 +2,8 @@ package gitChallenge4;
 
 public class Challenge4 {
 public static void main(String[] args) {
-	int a=10;
-	int b=20;
-	System.out.println(a+b);
+	int number1=10;
+	int number=20;
+	System.out.println(number1+number);
 }
 }
