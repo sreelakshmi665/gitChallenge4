5b5479900c07802dd9212b203adcfce97964f43f
sreelakshmi665 <16691a0589@mits.ac.in>
2021-02-08 10:23:53 +0530

    commit

--- gitChallenge4/src/gitChallenge4/Challenge4.java
+++ gitChallenge4/src/gitChallenge4/Challenge4.java
@@ -6,9 +6,7 @@ public static void main(String[] args) {
 	int number1=10;
 	int number=20;
 	System.out.println(number1+number);
-	int data1=10;
-	int data2=20;
-	System.out.println(data1+data2);
+
 
 }
 }
